bf21c9fe3e40ab17c95d7995294b6c7ec05bf8d4
Katie Busemeyer <kbusemeyer@yahoo.com>
2020-09-23 21:11:37 -0400

    testing if can send in props

--- src/core/main.js
+++ src/core/main.js
@@ -34,7 +34,7 @@ import * as constants from './constants';
  * @return {p5}                 a p5 instance
  */
 class p5 {
-  constructor(sketch, node, sync) {
+  constructor(sketch, node, sync, setupProps) {
     //////////////////////////////////////////////
     // PUBLIC p5 PROPERTIES AND METHODS
     //////////////////////////////////////////////
@@ -170,7 +170,7 @@ class p5 {
       text: false,
       grid: false,
       textLabel: false,
-      gridLabel: false
+      gridLabel: false,
     };
     this._setupDone = false;
     // for handling hidpi
@@ -186,7 +186,7 @@ class p5 {
     this._initializeInstanceVariables();
     this._defaultCanvasSize = {
       width: 100,
-      height: 100
+      height: 100,
     };
     this._events = {
       // keep track of user-events for unregistering later
@@ -206,7 +206,7 @@ class p5 {
       touchmove: null,
       touchend: null,
       resize: null,
-      blur: null
+      blur: null,
     };
     this._millisStart = -1;
 
@@ -216,6 +216,8 @@ class p5 {
 
     this._events.wheel = null;
     this._loadingScreenId = 'p5_loading';
+
+    this._setupProps = setupProps;
 
     // Allows methods to be registered on an instance that
     // are instance-specific.
@@ -276,12 +278,12 @@ class p5 {
         context.preload();
         this._runIfPreloadsAreDone();
       } else {
-        this._setup();
+        this._setup(this._setupProps);
         this._draw();
       }
     };
 
-    this._runIfPreloadsAreDone = function() {
+    this._runIfPreloadsAreDone = function () {
       const context = this._isGlobal ? window : this;
       if (context._preloadCount === 0) {
         const loadingScreen = document.getElementById(context._loadingScreenId);
@@ -296,7 +298,7 @@ class p5 {
       }
     };
 
-    this._decrementPreload = function() {
+    this._decrementPreload = function () {
       const context = this._isGlobal ? window : this;
       if (typeof context.preload === 'function') {
         context._setProperty('_preloadCount', context._preloadCount - 1);
@@ -304,7 +306,7 @@ class p5 {
       }
     };
 
-    this._wrapPreload = function(obj, fnName) {
+    this._wrapPreload = function (obj, fnName) {
       return (...args) => {
         //increment counter
         this._incrementPreload();
@@ -313,12 +315,12 @@ class p5 {
       };
     };
 
-    this._incrementPreload = function() {
+    this._incrementPreload = function () {
       const context = this._isGlobal ? window : this;
       context._setProperty('_preloadCount', context._preloadCount + 1);
     };
 
-    this._setup = () => {
+    this._setup = (setupProps) => {
       // Always create a default canvas.
       // Later on if the user calls createCanvas, this default one
       // will be replaced
@@ -345,7 +347,7 @@ class p5 {
       // Short-circuit on this, in case someone used the library in "global"
       // mode earlier
       if (typeof context.setup === 'function') {
-        context.setup();
+        context.setup(setupProps);
       }
 
       // unhide any hidden canvases that were created
@@ -474,7 +476,7 @@ class p5 {
 
         // call any registered remove functions
         const self = this;
-        this._registeredMethods.remove.forEach(f => {
+        this._registeredMethods.remove.forEach((f) => {
           if (typeof f !== 'undefined') {
             f.call(self);
           }
@@ -503,7 +505,7 @@ class p5 {
     };
 
     // call any registered init functions
-    this._registeredMethods.init.forEach(function(f) {
+    this._registeredMethods.init.forEach(function (f) {
       if (typeof f !== 'undefined') {
         f.call(this);
       }
@@ -592,7 +594,7 @@ class p5 {
     this._colorMaxes = {
       rgb: [255, 255, 255, 255],
       hsb: [360, 100, 100, 1],
-      hsl: [360, 100, 100, 1]
+      hsl: [360, 100, 100, 1],
     };
 
     this._downKeys = {}; //Holds the key codes of currently pressed keys
@@ -626,7 +628,7 @@ class p5 {
       // albeit one that is very unlikely to be used:
       //
       //   https://developer.mozilla.org/en-US/docs/Web/API/Window/print
-      print: true
+      print: true,
     };
 
     return (prop, value) => {
@@ -666,12 +668,12 @@ class p5 {
                 configurable: true,
                 enumerable: true,
                 value: newValue,
-                writable: true
+                writable: true,
               });
               log(
                 `You just changed the value of "${prop}", which was a p5 function. This could cause problems later if you're not careful.`
               );
-            }
+            },
           });
         } catch (e) {
           log(
@@ -724,7 +726,7 @@ p5.prototype._preloadMethods = {
   loadTable: p5.prototype,
   loadFont: p5.prototype,
   loadModel: p5.prototype,
-  loadShader: p5.prototype
+  loadShader: p5.prototype,
 };
 
 p5.prototype._registeredMethods = { init: [], pre: [], post: [], remove: [] };
